31e9efbeeb2d2f01d6d0b23f1133a90435bd0e3b
gs866812 <gooogle.sarwar@gmail.com>
2024-09-19 09:24:13 +0600

    try semo mail

--- src/app/api/getQuoteMail/route.js
+++ src/app/api/getQuoteMail/route.js
@@ -19,7 +19,7 @@ export async function POST(req) {
 
     // Setup Nodemailer transporter
     const transporter = nodemailer.createTransport({
-      host: 'webbriks.com',
+      host: 'gsarwar.com',
       port: 587,
       auth: {
         user: process.env.EMAIL_USER,
@@ -41,7 +41,7 @@ export async function POST(req) {
     // Define the email options
     const mailOptions = {
       from: `"${fullName}" <${email}>`,
-      to: 'info@webbriks.com',
+      to: 'gooogle.sarwar@gmail.com',
       subject: 'New Quote Request',
       text: mailMessage,
     };
